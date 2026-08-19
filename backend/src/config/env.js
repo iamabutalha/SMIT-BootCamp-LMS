7ccc6ec63ac87de8e34c223628575f6f5f45fdd0
iamabutalha <maabutalhakhalil@gmail.com>
2026-08-19 11:01:29 +0500

    cors issue fix

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -16,5 +16,5 @@ module.exports = {
   mongoUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1d",
-  corsOrigin: process.env.CLIENT_URL || "http://localhost:5173",
+  corsOrigin: process.env.CLIENT_URL || "https://smit-boot-camp-lms.vercel.app",
 };
